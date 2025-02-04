578b70236adbe422465ac2287ab0710ee47e138c
Tom5401 <tom.nielen@hotmail.nl>
2025-02-04 16:53:58 +0100

    ctrl C fix

--- src/keyboard_valve_control/keyboard_valve_control/keyboard_valve_controller.py
+++ src/keyboard_valve_control/keyboard_valve_control/keyboard_valve_controller.py
@@ -8,17 +8,17 @@ import sys
 class KeyboardValveController(Node):
     def __init__(self):
         super().__init__('keyboard_valve_controller')
-
+        
         self.get_logger().info('Initializing Keyboard Valve Controller Node')
-
+        
         # Define key mapping for valve numbers
         self.key_map = {
             '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '0': 10,
             'q': 11, 'w': 12, 'e': 13, 'r': 14, 't': 15, 'y': 16
         }
-
+        
         self.current_valve = None  # Track currently open valve
-
+        
         # Create publisher
         self.control_publisher = self.create_publisher(String, 'control_valve', 10)
 
@@ -32,23 +32,26 @@ class KeyboardValveController(Node):
             tty.setraw(sys.stdin.fileno())
             while True:
                 key = sys.stdin.read(1)  # Read single character
+                if key == '\x03':  # Handle Ctrl+C
+                    raise KeyboardInterrupt
                 if key in self.key_map:
                     self.toggle_valve(self.key_map[key])
         except KeyboardInterrupt:
             self.get_logger().info('Shutting down keyboard valve controller')
         finally:
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+            rclpy.shutdown()
 
     def toggle_valve(self, valve_number):
         """ Toggle the specified valve, ensuring only one is open at a time. """
         msg = String()
-
+        
         # Close the currently open valve (if any)
         if self.current_valve is not None:
             msg.data = f'{self.current_valve}:OFF'
             self.control_publisher.publish(msg)
             self.get_logger().info(f'Closed valve {self.current_valve}')
-
+        
         # Open the new valve only if it's different from the currently open one
         if self.current_valve != valve_number:
             msg.data = f'{valve_number}:ON'
