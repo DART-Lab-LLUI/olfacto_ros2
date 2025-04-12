8ce2c3e70e6a968ffce4b17279bc273d0affdcd0
Tom5401 <tom.nielen@hotmail.nl>
2025-04-12 14:22:19 +0200

    Fixed address

--- src/sensor_output/sensor_output/pid_publisher.py
+++ src/sensor_output/sensor_output/pid_publisher.py
@@ -10,7 +10,7 @@ class PIDPublisher(Node):
         self.publisher = self.create_publisher(UInt16, '/pid_output', 10)
 
         # Serial connection
-        self.ser = serial.Serial('/dev/ttyACM0', 500000, timeout=0.01)
+        self.ser = serial.Serial('/dev/ttyACM1', 500000, timeout=0.01)
         self.buffer = []
         self.samples_per_publish = 10
 
