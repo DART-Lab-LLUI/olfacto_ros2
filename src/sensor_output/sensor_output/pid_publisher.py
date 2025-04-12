bde84f1a3e1f7597f733da81c5cf9595b5959f6d
Tom5401 <tom.nielen@hotmail.nl>
2025-04-12 14:14:42 +0200

    Fix sample rate with fixed timer

--- src/sensor_output/sensor_output/pid_publisher.py
+++ src/sensor_output/sensor_output/pid_publisher.py
@@ -17,22 +17,32 @@ class PIDPublisher(Node):
         self.samples_per_publish = 10  # Average 10 samples for 1 kHz publish
 
         # Call self.read_serial() every 0.5 ms
-        self.create_timer(0.0005, self.read_serial)
+        self.create_timer(0.001, self.publish_average)
 
-    def read_serial(self):
-        while self.ser.in_waiting >= 2:
-            raw = self.ser.read(2)
-            if len(raw) == 2:
-                sample = struct.unpack('<H', raw)[0]
-                self.buffer.append(sample)
+    # Add this method:
+    def publish_average(self):
+        if self.buffer:
+            avg_val = int(sum(self.buffer) / len(self.buffer))
+            self.buffer.clear()
+            msg = UInt16()
+            msg.data = avg_val
+            self.publisher.publish(msg)
 
-                if len(self.buffer) >= self.samples_per_publish:
-                    avg_val = int(sum(self.buffer) / len(self.buffer))
-                    self.buffer.clear()
 
-                    msg = UInt16()
-                    msg.data = avg_val
-                    self.publisher.publish(msg)
+#    def read_serial(self):
+#        while self.ser.in_waiting >= 2:
+#            raw = self.ser.read(2)
+#            if len(raw) == 2:
+#                sample = struct.unpack('<H', raw)[0]
+#                self.buffer.append(sample)
+
+#                if len(self.buffer) >= self.samples_per_publish:
+#                    avg_val = int(sum(self.buffer) / len(self.buffer))
+#                    self.buffer.clear()
+
+#                    msg = UInt16()
+#                    msg.data = avg_val
+#                    self.publisher.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
