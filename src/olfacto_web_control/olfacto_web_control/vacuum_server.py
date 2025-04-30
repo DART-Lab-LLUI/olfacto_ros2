79ba62d29511e4ee35af127b923a88dc5af8390b
Tom5401 <tom.nielen@hotmail.nl>
2025-04-30 11:04:15 +0200

    Tune

--- src/olfacto_web_control/olfacto_web_control/vacuum_server.py
+++ src/olfacto_web_control/olfacto_web_control/vacuum_server.py
@@ -32,7 +32,7 @@ class OlfactometerController(Node):
         #self.boost_duration = 0.2
         #self.odr_boost = 15.0
         #self.ctrl_boost = 16.0
-        self.odor_multiplier = 1.4
+        self.odor_multiplier = 1.05
         self.preload_delay = 2.0
         self.boost_duration = 0.6
         self.odr_boost = 10.0
